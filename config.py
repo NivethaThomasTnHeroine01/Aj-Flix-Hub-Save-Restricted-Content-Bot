f1b639a424f4f4828f2f59323460e784f07b94ec
NivethaThomasTnHeroine01 <nivethathomastnheroine01@gmail.com>
2025-03-13 01:05:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,17 +13,17 @@ YTUB_COOKIES = """
 # write here yt cookies
 """
 
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
-MONGO_DB = getenv("MONGO_DB", "")
-LOG_GROUP = getenv("LOG_GROUP", "")
-CHANNEL_ID = int(getenv("CHANNEL_ID", ""))
-FREEMIUM_LIMIT = int(getenv("FREEMIUM_LIMIT", "0"))
-PREMIUM_LIMIT = int(getenv("PREMIUM_LIMIT", "500"))
-WEBSITE_URL = getenv("WEBSITE_URL", "upshrink.com")
-AD_API = getenv("AD_API", "52b4a2cf4687d81e7d3f8f2b7bc2943f618e78cb")
+API_ID = int(getenv("API_ID", "27066819"))
+API_HASH = getenv("API_HASH", "823a68bafea9f46ab74f92656391746b")
+BOT_TOKEN = getenv("BOT_TOKEN", "7884089947:AAHncP79pvuJLF5imBGl5X4BLMa6XDAKpcA")
+OWNER_ID = list(map(int, getenv("OWNER_ID", "6001367891").split()))
+MONGO_DB = getenv("MONGO_DB", "mongodb+srv://ramsaranhero1:EMVwKybGR1lBF5DZ@cluster0.tst47tx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+LOG_GROUP = getenv("LOG_GROUP", "-1002387429617")
+CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002399708847"))
+FREEMIUM_LIMIT = int(getenv("FREEMIUM_LIMIT", "10"))
+PREMIUM_LIMIT = int(getenv("PREMIUM_LIMIT", "5000"))
+WEBSITE_URL = getenv("WEBSITE_URL", "modijiurl.com")
+AD_API = getenv("AD_API", "2c27efcec826e037d3a3b01821da3fb0e824ded4")
 STRING = getenv("STRING", None)
 YT_COOKIES = getenv("YT_COOKIES", YTUB_COOKIES)
 DEFAULT_SESSION = getenv("DEFAUL_SESSION", None)  # added old method of invite link joining
